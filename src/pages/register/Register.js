d2036a0a7355f893d7a6ce7cd710318b3343c71a
Nomddaay <92080450+Nomddaay@users.noreply.github.com>
2023-06-27 22:48:16 +0700

    install environments

---
+++ src/pages/register/Register.js
@@ -0,0 +1,18 @@
+// import { useState } from 'react';
+// import { useNavigate } from "react-router-dom";
+import './Register.css';
+
+const Register = () => {
+    // const [name, setName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+    // const navigate = useNavigate();
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Register;
