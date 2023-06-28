60f6b48369202126e6b56d430b06c9063ca273d2
Nomddaay <92080450+Nomddaay@users.noreply.github.com>
2023-06-28 16:59:35 +0700

    done UI registerpage

--- frontend/src/pages/register/Register.js
+++ frontend/src/pages/register/Register.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 // import { useNavigate } from "react-router-dom";
 import './Register.css';
 import Button from '../../components/btns/login_btn/ButtonRegister';
@@ -7,17 +8,30 @@ import Button from '../../components/btns/login_btn/ButtonRegister';
 const Register = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [matchPassword, setMatchPassword] = useState("");
     // const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault();   
 
     };
 
     return(
         <div className='body'>
             <div className='card'>
-                <div className='title'>VOTTE</div>
+                <Link to="/" 
+                    className='title'
+                    style={{
+                        display: 'flex',
+                        textDecoration: 'none',
+                        fontWeight: 700,
+                        fontSize: 30,
+                        justifyContent: 'center',
+                        marginTop: 25,
+                        marginBottom: 10,
+                    }}>
+                    VOTTE
+                </Link>
                 <form onSubmit={handleSubmit} className='content'>
                     <div className='label_name'>Email</div>
                     <input
@@ -39,10 +53,11 @@ const Register = () => {
                     <input
                         className='input'
                         type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        value={matchPassword}
+                        onChange={(e) => setMatchPassword(e.target.value)}
                     />
                     <Button />
+                    <div className='text'>Have account? <a href='/'>Login</a></div>
                 </form> 
             </div>
         </div>
