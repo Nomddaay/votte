28c909d1bec3904ea11fceeb08c5b3224d0ac98e
Nomddaay <92080450+Nomddaay@users.noreply.github.com>
2023-07-01 12:10:42 +0700

    done UI navbar

--- frontend/src/components/header/navbar/Navbar.js
+++ frontend/src/components/header/navbar/Navbar.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 
-const Navbar = () => {
+const NavBar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav_bar'>
+      <div className='nav_bar_options'>
+        <ul>
+          <li>
+            <Link to='/' className='nav_items'>SONGS</Link>
+          </li>
+          <li>
+            <Link to='/' className='nav_items'>SPORTS</Link>
+          </li>
+          <li>
+            <Link to='/' className='nav_items'>MOVIES</Link>
+          </li>
+          <li>
+            <Link to='/' className='nav_items'>BOOKS</Link>
+          </li>
+          <li>
+            <Link to='/' className='nav_items'>BLOG</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
-export default Navbar;
+export default NavBar;
--- frontend/src/pages/homepage/HomePage.js
+++ frontend/src/pages/homepage/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import TitleBar from '../../components/header/titleBar/TitleBar';
+import NavBar from '../../components/header/navbar/NavBar';
 
 const HomePage = () => {
   return (
-    <TitleBar />
+    <>
+      <TitleBar />
+      <NavBar />
+    </>
   )
 }
 
-export default HomePage
+export default HomePage;
